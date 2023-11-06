70d231d4b74db878a0a6c0ad1d4225c2aaf9674e
Josh Cubero <jnatc1@gmail.com>
2023-11-06 09:35:57 -0500

    Updated Server and Client

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 import { config } from 'dotenv';
-import { connectToServer, getDb } from './db/conn.js'; // Import connectToServer function
+import { connectToServer, getDb } from './db/conn.js'; 
 import scenarioRoutes from './routes/scenario.js'; 
 const app = express();
 config({ path: './config.env' });
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 app.use(scenarioRoutes);
 
 app.listen(port, () => {
-  const dbo = connectToServer(function (err) {
+  connectToServer(function (err) {
     if (err) console.error(err);
-   });
+  });
   console.log(`Server is running on port: ${port}`);
 });
