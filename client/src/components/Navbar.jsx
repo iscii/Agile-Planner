2b9075033216e1522122f3616e382a8258665cb6
iscii <issac.zheng9869@gmail.com>
2023-11-26 13:18:57 -0500

    navbar spacing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,27 +7,27 @@ import { NavLink } from "react-router-dom";
 import '../style/navbar.scss'
  
 export default function Navbar() {
- return (
-   <div>
-     <nav className="navbar navbar-expand-lg d-flex flex-row justify-content-between">
-        <NavLink className="navbar-brand" to="/">
-          <img style={{"width" : 25 + '%'}} src={agile}></img>
-        </NavLink>
-        <div className="navbar-right collapse navbar-collapse flex-grow-0 mr-5" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/create">
-                  Create Record
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/logout">
-                  Logout
-                </NavLink>
-              </li>
-          </ul>
-       </div>
-     </nav>
-   </div>
- );
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg d-flex flex-row justify-content-between">
+          <NavLink className="navbar-brand" to="/">
+            <img style={{"width" : 25 + '%'}} src={agile}></img>
+          </NavLink>
+          <div className="navbar-right collapse navbar-collapse flex-grow-0 mr-5" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/create">
+                    Create Record
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/logout">
+                    Logout
+                  </NavLink>
+                </li>
+            </ul>
+        </div>
+      </nav>
+    </div>
+  );
 }
